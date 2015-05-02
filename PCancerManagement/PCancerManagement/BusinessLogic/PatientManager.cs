edcf382c61411039365463ebbca333253aa1c2b3
sohel364 <sohel.official@gmail.com>
2015-05-02 15:18:54 +0600

    mngr

--- PCancerManagement/PCancerManagement/BusinessLogic/PatientManager.cs
+++ PCancerManagement/PCancerManagement/BusinessLogic/PatientManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PCancerManagement.DataObjects;
 
 namespace PCancerManagement.BusinessLogic
 {
     class PatientManager
     {
+
+        public static void InsertPatient(Patient patientObject){
+            if (patientObject != null) {
+                String sqlInsertQuery = "";
+            }
+        }
     }
 }

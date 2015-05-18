3ada3ed16afeeeeb79082bd8b348d83db3929819
sohel364 <sohel.official@gmail.com>
2015-05-18 08:58:35 +0600

    Doctor.cs Refferal.cs

---
+++ PCancerManagement/DataObjects/Doctor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class Doctor
+    {
+        private string _doctor_id;
+        private string _doctor_name;
+        private string _doctor_details;
+
+        public string Doctor_id
+        {
+            get { return _doctor_id; }
+            set { _doctor_id = value; }
+        }
+
+        public string Doctor_name
+        {
+            get { return _doctor_name; }
+            set { _doctor_name = value; }
+        }
+
+        public string Doctor_details
+        {
+            get { return _doctor_details; }
+            set { _doctor_details = value; }
+        }
+
+        public Doctor() { 
+
+        }
+    }
+}
---
+++ PCancerManagement/DataObjects/Refferal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class Refferal
+    {
+        private string _refferal_id;
+        private string _details;
+        private string _reffered_by;
+
+        public string Refferal_id
+        {
+            get { return _refferal_id; }
+            set { _refferal_id = value; }
+        }
+
+
+        public string Reffered_by
+        {
+            get { return _reffered_by; }
+            set { _reffered_by = value; }
+        }
+
+
+        public string Details
+        {
+            get { return _details; }
+            set { _details = value; }
+        }
+    }
+}

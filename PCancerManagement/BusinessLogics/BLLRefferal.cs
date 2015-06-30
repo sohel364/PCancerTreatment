9ac8ee9a821b39e1db07bad1c6e9cd091556ced7
sohel364 <sohel.official@gmail.com>
2015-07-01 01:36:21 +0600

    New File Added in BusinessLogics.csproj BLLRefferal.cs Added

---
+++ PCancerManagement/BusinessLogics/BLLRefferal.cs
@@ -0,0 +1,43 @@
+﻿using DataObjects;
+using PCancerDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogics
+{
+    public class BLLRefferal
+    {
+    
+        /// <summary>
+        /// Method to Insert Patient Refferal
+        /// </summary>
+        /// <param name="refferal"></param>
+    
+        public static void Insert(Refferal refferal)
+        {
+            SqlParameter[] param = new SqlParameter[3];
+
+            param[0] = new SqlParameter("@referreal_id", SqlDbType.NVarChar, 15);
+            param[0].Direction = ParameterDirection.Output;
+            param[1] = new SqlParameter("@reffered_by_name", refferal.Reffered_by);
+            param[2] = new SqlParameter("@details", refferal.Details);
+            
+
+            try
+            {
+               refferal.Refferal_id = DBHelper.ExecuteReturnIDStoredProcedure("insert_refferal", param);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
